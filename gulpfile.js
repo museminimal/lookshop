4300c69db791ff5ee9655029a09eb991563f6a2e
Hanna <muse.minimal@gmail.com>
2020-06-06 14:59:54 +0300

    'start'

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const { watch, src, dest, series, parallel } = require('gulp');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const del = require('del');
+
+function scssCompile () {
+  return src('app/scss/main.scss')
+        .pipe(sass({ noCache: true, style: 'compressed' }).on('error', sass.logError))
+        .pipe(concat('main.css'))
+        .pipe(dest('dist/css'))
+}
+
+function htmlMoving () {
+    return src('app/index.html')
+          .pipe(dest('dist'))
+
+}
+
+function jsCompile () {
+  return src('app/js/main.js')
+        .pipe(concat('main.js'))
+        .pipe(dest('dist/js'))
+}
+
+function clean () {
+  return del('dist');
+}
+
+function syncBrowsers () {
+  browserSync.init({
+    server: {
+      baseDir: 'dist/'
+    },
+    notify: false,
+    port: 3000,
+    open: false
+  });
+}
+
+function watchFiles () {
+  syncBrowsers();
+
+  /* WATCH HTML */
+  watch('app/index.html').on('change', htmlMoving);
+  watch('dist/index.html').on('change', browserSync.reload);
+
+  /* WATCH STYLES */
+  watch('app/scss/**/*.scss').on('change', scssCompile);
+  watch('dist/css/main.css').on('change', browserSync.reload);
+
+  /* WATCH JS */
+  watch('app/js/main.js').on('change', jsCompile);
+}
+
+exports.watch = series(clean, parallel(scssCompile, jsCompile, htmlMoving), watchFiles);
